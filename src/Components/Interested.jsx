9901f4c4ca3bb3e4eec8dbf9626329d0fcb74640
dhanwanth-dh <dhanwanth.dj.13@gmail.com>
2026-01-08 21:06:12 +0530

    Interested page changed

--- src/Components/Interested.jsx
+++ src/Components/Interested.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Interested = () => {
     return (
-        <div className='z-100'>Interested</div>
+        <div className='z-100'>
+            <h1>Interested</h1>
+            <p>Interested in working together? We should queue up a time to chat. I’ll buy the coffee.</p>
+        </div>
     )
 }
 
