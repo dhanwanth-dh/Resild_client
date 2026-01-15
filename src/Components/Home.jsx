817f9c56202342bba2bc6fe0ba44733f6698699a
dhanwanth-dh <dhanwanth.dj.13@gmail.com>
2026-01-15 05:33:51 +0530

    UI altered

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,12 +22,12 @@ const Home = () => {
             <div className='z-100 mt-0'>
                 <div className='p-5 flex flex-col gap-5 w-80 '>
 
-                    {/* <div className='flex flex-row justify-between gap-10'> */}
-                    <button className='h-10 p-1 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl rounded-tl-3xl border-2 border-black bg-white shadow-xl/60 opacity-90
+                    <div className='flex flex-row justify-between gap-10'>
+                        <button className='h-10 p-1 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl rounded-tl-3xl border-2 border-black bg-white shadow-xl/60 opacity-90
                         transition duration-100 hover:bg-gray-600'><Link to="/skills">Update skills</Link></button>
-                    <button className='h-10 p-1 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl rounded-tl-3xl border-2 border-black bg-white shadow-xl/60 opacity-90
+                        <button className='h-10 p-1 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl rounded-tl-3xl border-2 border-black bg-white shadow-xl/60 opacity-90
                         transition duration-100 hover:bg-gray-600'>Interested</button>
-                    {/* </div> */}
+                    </div>
 
                     <button className='h-10 p-1 rounded-bl-3xl rounded-br-3xl rounded-tr-3xl rounded-tl-3xl border-2 border-black bg-white shadow-xl/60 opacity-90
                     transition duration-100 hover:bg-gray-600' onClick={markInterested}>Mark interested</button>
